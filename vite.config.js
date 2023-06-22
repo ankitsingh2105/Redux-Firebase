4bf3bcee88ea17f764f98d34ea445b893cdd5a3a
Ankit <ankitchauhan21500@gmail.com>
2023-06-23 03:31:35 +0530

    great progress

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
   build: {
     target: ['edge90', 'chrome90', 'firefox90', 'safari15']
   }
 })
-// plugins: [react()],
